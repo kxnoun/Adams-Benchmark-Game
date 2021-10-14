fccdf3c87f59f0b5649d066b9376a82ee1d8b206
Adam Kanoun <adam.kanoun@gmail.com>
2021-10-14 19:28:04 -0400

    Created Button, added buttons to game and fixed main

---
+++ Button.py
@@ -0,0 +1,76 @@
+"""
+Adam Kanoun
+This is where everything related to class Button is stored.
+"""
+from typing import Tuple, Optional, Union
+import pygame
+
+
+class Button:
+    """
+    A class representing a button in pygame.
+    """
+    text: str
+    text_color: Tuple[int, int, int]
+    font_size: int
+    color: Tuple[int, int, int]
+    x: Union[int, float]
+    y: Union[int, float]
+    width: Union[int, float]
+    height: Union[int, float]
+
+    def __init__(self, color: Tuple[int, int, int], x: Union[int, float],
+                 y: Union[int, float], width: Union[int, float],
+                 height: Union[int, float], font_size: int, text: str = "",
+                 text_color: Tuple[int, int, int] = (0, 0, 0),
+                 outline: Optional[Tuple[int, int, int]] = None) -> None:
+        """
+        Initialize a button.
+        """
+        self.text = text
+        self.text_color = text_color
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.font_size = font_size
+        self.outline = outline
+
+    def draw_button(self, screen: pygame.Surface) -> None:
+        """
+        Draw a button on the screen.
+        """
+        if self.outline is not None:
+            # Draw a rectangle with color <outline> that is bigger than width
+            # and height, to make it seem like it is an outline.
+            pygame.draw.rect(screen, self.outline, (self.x - 2, self.y - 2,
+                                               self.width + 4,
+                                               self.height + 4), 0)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width,
+                                              self.height), 0)
+
+    def draw_text(self, screen: pygame.Surface) -> None:
+        """
+        Draw the text for the button.
+        """
+        if self.text != '':
+            button_font = pygame.font.Font('freesansbold.ttf', self.font_size)
+            text = button_font.render(self.text, True, self.text_color)
+            text_rect = text.get_rect()
+            text_rect.center = ((self.x + (self.width / 2)), self.y + (self.height / 2))
+            screen.blit(text, text_rect)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        self.draw_button(screen)
+        self.draw_text(screen)
+
+    def is_hover(self, position: Tuple[int, int]) -> bool:
+        """
+        Returns whether or not the mouse is hovering over the button (when the
+        mouse is at <position> (x, y)).
+        """
+        if self.x < position[0] < self.x + self.width and \
+                self.y < position[1] < self.y + self.height:
+            return True
+        return False
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ Adam Kanoun
 This is where the benchmark game runs!
 """
 import pygame
-from typing import Tuple
+from typing import Tuple, List
+from Button import Button
 
+# COLOR CONSTANTS
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -12,6 +14,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (192, 192, 192)
 LIGHT_YELLOW = (255, 255, 158)
+LIGHT_BROWN = (196, 164, 132)
 
 
 class Screen:
@@ -21,19 +24,45 @@ class Screen:
     SCREEN_WIDTH: int = 1280
     SCREEN_HEIGHT: int = 720
     BACKGROUND: Tuple[int, int, int] = LIGHT_YELLOW
-    running: bool = True
     game_running: bool = True
-    intro: bool = True
+    intro_running: bool = True
+    main_running: bool = False
+    reaction_running: bool = False
+    number_running: bool = False
+    verbal_running: bool = False
+    main_menu_buttons: List[Button]
 
     def __init__(self) -> None:
         """
         Initialize the Screen.
+        === Representation Invariants ===
+        Exactly one of the following attributes may be True at a time.
+        intro_running, main_running, reaction_running, number_running,
+        verbal_running.
         """
         pygame.init()
 
         self.screen = pygame.display.set_mode(
             (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-
+        # Create buttons
+        self.main_menu_buttons = []
+        reaction_time_button = Button(LIGHT_BROWN,
+                                      (self.SCREEN_WIDTH / 2) - 500,
+                                      (self.SCREEN_HEIGHT / 3), 250, 250, 30,
+                                      "Reaction Time", BLACK, BLACK)
+        number_memory_button = Button(LIGHT_BROWN,
+                                      (self.SCREEN_WIDTH / 2) - 125,
+                                      self.SCREEN_HEIGHT / 3, 250, 250, 30,
+                                      "Number Memory", BLACK, BLACK)
+        verbal_memory_button = Button(LIGHT_BROWN,
+                                      (self.SCREEN_WIDTH / 2) + 250,
+                                      self.SCREEN_HEIGHT / 3, 250, 250, 30,
+                                      "Verbal Memory", BLACK, BLACK)
+        back_button1 = Button(BLACK, 15, 20, 100, 30, 20, "BACK", WHITE, WHITE)
+        self.main_menu_buttons.append(reaction_time_button)
+        self.main_menu_buttons.append(number_memory_button)
+        self.main_menu_buttons.append(verbal_memory_button)
+        self.main_menu_buttons.append(back_button1)
         pygame.display.flip()
 
     @staticmethod
@@ -44,58 +73,131 @@ class Screen:
         screen_text = font.render(text, True, color)
         return screen_text, screen_text.get_rect()
 
-    def draw_window(self):
+    def draw_background(self) -> None:
         """
-        Draw the window
+        Simply draws the background color.
         """
         self.screen.fill(self.BACKGROUND)
-        pygame.display.flip()
 
-    def game_intro(self):
+    def draw_game_intro(self) -> None:
         """
-        Draw the first home scene.
+        A helper method for game_intro.
+        This method draws the text and background for the game intro.
         """
-        while self.intro:
-            self.screen.fill(self.BACKGROUND)
-            intro_font = pygame.font.Font('freesansbold.ttf', 100)
-            sub_font = pygame.font.Font('freesansbold.ttf', 35)
-            text, text_rect = self.text_obj("Adam's Benchmark Tests",
-                                            BLACK, intro_font)
-            subtext, subtext_rect = self.text_obj("Press Space to Begin", GREY,
-                                                  sub_font)
-            text_rect.center = (self.SCREEN_WIDTH / 2), (self.SCREEN_HEIGHT / 2)
-            subtext_rect.center = (self.SCREEN_WIDTH / 2), \
-                                  (2 * self.SCREEN_HEIGHT / 3)
-            self.screen.blit(text, text_rect)
-            self.screen.blit(subtext, subtext_rect)
+        intro_font = pygame.font.Font('freesansbold.ttf', 100)
+        sub_font = pygame.font.Font('freesansbold.ttf', 35)
+        text, text_rect = self.text_obj("Adam's Benchmark Tests", BLACK,
+                                        intro_font)
+        subtext, subtext_rect = self.text_obj("Press Space to Begin", GREY,
+                                              sub_font)
+        text_rect.center = (self.SCREEN_WIDTH / 2), (self.SCREEN_HEIGHT / 2)
+        subtext_rect.center = (self.SCREEN_WIDTH / 2), \
+                              (2 * self.SCREEN_HEIGHT / 3)
+        self.screen.blit(text, text_rect)
+        self.screen.blit(subtext, subtext_rect)
+
+    def game_intro(self) -> None:
+        """
+        Runs the opening scene.
+        """
+        while self.intro_running and self.game_running:
+            self.draw_background()
+            self.draw_game_intro()
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        self.intro = False
-                        self.running = False
+                        self.intro_running = False
+                        self.game_running = False
                     elif event.key == pygame.K_SPACE:
-                        self.intro = False
+                        self.intro_running = False
                 if event.type == pygame.QUIT:
-                    self.intro = False
-                    self.running = False
+                    self.intro_running = False
+                    self.game_running = False
 
-    def main(self):
+    def draw_main_menu_text(self) -> None:
         """
-        The main loop of the code.
+        Draws the main menu text
         """
-        while self.running:
+        main_menu_font = pygame.font.Font('freesansbold.ttf', 60)
+        text, text_rect = self.text_obj("Choose any game!", BLACK,
+                                        main_menu_font)
+        text_rect.center = (self.SCREEN_WIDTH / 2), \
+                           (self.SCREEN_HEIGHT / 12)
+        self.screen.blit(text, text_rect)
+
+    def draw_main_menu_buttons(self) -> None:
+        """
+        Draws the main menu buttons.
+        """
+        for button in self.main_menu_buttons:
+            orig_button_color = button.color
+            if button.is_hover(pygame.mouse.get_pos()):
+                button.color = (button.color[0] + (255 -
+                                                   button.color[0]) * 0.3,
+                                button.color[1] + (255 -
+                                                   button.color[1]) * 0.3,
+                                button.color[2] + (255 -
+                                                   button.color[2]) * 0.3)
+            button.draw(self.screen)
+            button.color = orig_button_color
+
+    def press_main_menu_button(self):
+        pass
+
+    def main_menu(self) -> None:
+        """
+        Runs the main menu scene.
+        """
+        self.main_running = True
+        while self.main_running and self.game_running:
+            self.draw_background()
+            self.draw_main_menu_text()
+            self.draw_main_menu_buttons()
+            pygame.display.update()
             for event in pygame.event.get():
+                # Check mouse clicks and positions to determine if any
+                # running attributes must be changed.
+                # if self.reaction_running is True then run reaction_game
+                # elif self.number_running is True then run number_game
+                # elif self.verbal_running is True then run verbal_game
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        self.running = False
+                        self.main_running = False
+                        self.game_running = False
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    self.main_running = False
+                    self.game_running = False
 
-            self.draw_window()
+        # Use assertion to make sure representation invariants are working as
+        # they should.
+
+    def reaction_game(self) -> None:
+        """
+        Runs the reaction time benchmark/game.
+        """
+        pass
+
+    def number_game(self) -> None:
+        """
+        Runs the number memory benchmark/game.
+        """
+        pass
+
+    def verbal_game(self) -> None:
+        """
+        Runs the verbal memory benchmark/game.
+        """
+        pass
+
+    def run_game(self) -> None:
+        """
+        Runs the game
+        """
+        pass
 
 
 if __name__ == '__main__':
     screen = Screen()
     screen.game_intro()
-    screen.main()
+    screen.main_menu()
